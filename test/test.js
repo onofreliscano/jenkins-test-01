0ef08cab02c12d1a63e2afaf7b325761c4656d24
onofreliscano <onofreliscano@hotmail.com>
2021-09-24 03:27:45 -0400

    commit 003 - testing script

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function() {
+	it('respond with a company name', function(done){
+		request(app).get('/').expect('NativApps - Jenkins + Github', done);
+	})
+});
